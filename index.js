5eb2a62ea45d962795aede16e016939dcbe989fa
therealrinku <rinkunited2032@gmail.com>
2024-02-14 08:47:09 +0545

    chore: refactor index.js attempting to fix deploy error

--- index.js
+++ index.js
@@ -1,10 +1,11 @@
-//LOAD ENV
-require("dotenv").config();
-
 const express = require("express");
 const http = require("http");
 const cors = require("cors");
 const cookieParser = require("cookie-parser");
+const dotenv = require("dotenv");
+
+//LOAD ENV
+dotenv.config();
 
 const { db } = require("./database/db");
 
@@ -16,7 +17,7 @@ const app = express();
 
 const server = http.createServer(app);
 
-app.use(cors({ origin: ["http://localhost:3000"], credentials: true }));
+app.use(cors({ origin: ["http://localhost:3000", "https://robocreate.vercel.app"], credentials: true }));
 app.use(express.json());
 app.use(cookieParser());
 
@@ -25,5 +26,8 @@ const connectionsRoute = require("./routes/connections");
 
 app.use("/users", usersRoute);
 app.use("/connections", connectionsRoute);
+
+//first landing route
+app.get("/", (_, res) => res.status(200).send({ message: "Robocreate API is ready to serve." }));
 
 server.listen(process.env.PORT, () => console.log(`Express is running at port ${process.env.PORT}`));
