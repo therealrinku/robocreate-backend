fa6014373d88f911cf48cfc805d7d96c6e24d828
therealrinku <rinkunited2032@gmail.com>
2024-02-14 09:05:09 +0545

    chore: replaced bcrypt with bcryptjs

--- routes/users.js
+++ routes/users.js
@@ -1,7 +1,7 @@
 const router = require("express").Router();
 const { db } = require("../database/db");
 const jwt = require("jsonwebtoken");
-const bcrypt = require("bcrypt");
+const bcrypt = require("bcryptjs");
 
 const { verifyJWT } = require("../middlewares/verifyJWT");
 
