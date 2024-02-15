3e48956f32e09b841ecb80011920db42ebc914f9
therealrinku <rinkunited2032@gmail.com>
2024-02-15 07:57:09 +0545

    fix: cors error again damn

--- index.js
+++ index.js
@@ -17,7 +17,7 @@ const app = express();
 
 const server = http.createServer(app);
 
-app.use(cors({ origin: ["http://localhost:3000", "https://robocreate.vercel.app"], credentials: true }));
+app.use(cors({ origin: "*", credentials: true }));
 app.use(express.json());
 app.use(cookieParser());
 
