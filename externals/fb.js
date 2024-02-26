ae98551a57dde295bf2977b3f932f0b048150910
therealrinku <rinkunited2032@gmail.com>
2024-02-26 11:51:15 +0545

    refactor: cleanup fb.js

--- externals/fb.js
+++ externals/fb.js
@@ -1,38 +1,32 @@
-// =================================================================================
-// =================== HANDLE EVERYTHING FACEBOOK HERE =============================
-// =================================================================================
+/*
+Facebook API handler
+  long lived access token docs: https://developers.facebook.com/docs/facebook-login/guides/access-tokens/get-long-lived/
+  post docs: https://developers.facebook.com/docs/pages-api/posts/
+*/
 
-// DOCS FOR LONG LIVED ACCESS TOKEN: https://developers.facebook.com/docs/facebook-login/guides/access-tokens/get-long-lived/
-
-const { FB_GRAPH_API_BASE_URL: fbGraphApiBaseUrl, FB_APP_SECRET: fbAppSecret } = process.env;
+const { handleChannelApiCall } = require("../utils/api");
+const { constants } = require("../utils/constants");
 
 async function createPost(pageId, body, pageAccessToken) {
   if (!pageId || !body || !pageAccessToken) {
     throw new Error("Not enough info to create post");
   }
 
-  const responseJson = await (
-    await fetch(
-      `${fbGraphApiBaseUrl}/${pageId}/feed?fields=likes,comments.limit(10),shares,full_picture,permalink_url`,
-      {
-        method: "post",
-        headers: {
-          Authorization: `Bearer ${pageAccessToken}`,
-          "Content-Type": "application/json",
-        },
-        //https://developers.facebook.com/docs/pages-api/posts/
-        // expected body =>  {
-        //   "message":"your_message_text",
-        //   "link":"your_url",
-        //   "published":"false",
-        //   "scheduled_publish_time":"unix_time_stamp_of_a_future_date",
-        // }
-        body: JSON.stringify(body),
-      }
-    )
-  ).json();
+  const response = await handleChannelApiCall({
+    endpoint: `${pageId}/feed`,
+    bearerToken: pageAccessToken,
+    channel: "facebook",
+    method: "post",
+    body: body,
+  });
 
-  return responseJson;
+  const postId = await response.json()?.id;
+
+  if (!postId) {
+    throw new Error("Something went wrong while publishing the post.");
+  }
+
+  return postId;
 }
 
 async function getPagePosts(pageId, pageAccessToken) {
@@ -40,18 +34,14 @@ async function getPagePosts(pageId, pageAccessToken) {
     throw new Error("Not enough info to get page posts");
   }
 
-  const postsRespJson = await (
-    await fetch(
-      `${fbGraphApiBaseUrl}/${pageId}/feed?fields=likes,comments.limit(10),shares,full_picture,permalink_url,message,created_time`,
-      {
-        headers: {
-          Authorization: `Bearer ${pageAccessToken}`,
-        },
-      }
-    )
-  ).json();
+  const response = await handleChannelApiCall({
+    endpoint: `${pageId}/feed`,
+    queryParamString: `fields=likes,comments.limit(10),shares,full_picture,permalink_url,message,created_time`,
+    bearerToken: pageAccessToken,
+    channel: "facebook",
+  });
 
-  return postsRespJson;
+  return await response.json();
 }
 
 async function getMe(shortLivedAccessToken) {
@@ -59,13 +49,14 @@ async function getMe(shortLivedAccessToken) {
     throw new Error("Invalid access token");
   }
 
-  const meResp = await fetch(`${fbGraphApiBaseUrl}/me?fields=id,name`, {
-    headers: {
-      Authorization: `Bearer ${shortLivedAccessToken}`,
-    },
+  const response = await handleChannelApiCall({
+    endpoint: `me`,
+    queryParamString: `fields=id,name`,
+    bearerToken: shortLivedAccessToken,
+    channel: "facebook",
   });
-  const meRespJson = await meResp.json();
-  return meRespJson;
+
+  return await response.json();
 }
 
 async function getUserLongLivedAccessToken(shortLivedAccessToken) {
@@ -73,13 +64,21 @@ async function getUserLongLivedAccessToken(shortLivedAccessToken) {
     throw new Error("Invalid access token");
   }
 
-  const userLongLivedAccessTokenResp = await fetch(`${fbGraphApiBaseUrl}/oauth/access_token?  
-          grant_type=fb_exchange_token&          
-          client_id=881256046505003&
-          client_secret=${fbAppSecret}&
-          fb_exchange_token=${shortLivedAccessToken}`);
+  const response = await handleChannelApiCall({
+    endpoint: `oauth/access_token`,
+    queryParamString: `grant_type=fb_exchange_token&          
+    client_id=${constants.fbAppId}&
+    client_secret=${constants.fbAppSecret}&
+    fb_exchange_token=${shortLivedAccessToken}`,
+    channel: "facebook",
+  });
 
-  const longLivedAccessToken = (await userLongLivedAccessTokenResp.json()).access_token;
+  const longLivedAccessToken = (await response.json())?.access_token;
+
+  if (!longLivedAccessToken) {
+    throw new Error("Something went wrong, please try again later");
+  }
+
   return longLivedAccessToken;
 }
 
@@ -88,18 +87,21 @@ async function getFirstPage(userLongLivedAccessToken, appScopedUserId) {
     throw new Error("Not enough data provided");
   }
 
-  const resp = await fetch(`${fbGraphApiBaseUrl}/${appScopedUserId}/accounts?
-  access_token=${userLongLivedAccessToken}`);
+  const response = await handleChannelApiCall({
+    endpoint: `${appScopedUserId}/accounts`,
+    queryParamString: `access_token=${userLongLivedAccessToken}`,
+    channel: "facebook",
+  });
 
-  // supports one page only for now >><<<
-  // multi pages support coming soon
-  const jsonResp = await resp.json();
+  const jsonResponse = await response.json();
 
-  if (jsonResp.data.length === 0) {
-    throw new Error("Please provide at full page access.");
+  if (jsonResponse.data?.length === 0) {
+    throw new Error("Please provide the full access.");
   }
 
-  const firstPage = jsonResp.data[0];
+  // we just return the first found page here
+  // i.e user can connect one fb page at a time
+  const firstPage = jsonResponse.data[0];
   return firstPage;
 }
 
---
+++ utils/constants.js
@@ -0,0 +1,7 @@
+const constants = {
+  fbGraphApiBaseUrl: process.env.FB_GRAPH_API_BASE_URL,
+  fbAppSecret: process.env.FB_APP_SECRET,
+  fbAppId: process.env.FB_APP_ID,
+};
+
+module.exports = { constants };
