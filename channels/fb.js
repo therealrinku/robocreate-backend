b31f893d211d54cd38ced4b9f057c6c6be70bc5b
therealrinku <rinkunited2032@gmail.com>
2024-02-27 11:46:55 +0545

    chore: refactor to fix vercel deploy error :>

--- channels/fb.js
+++ channels/fb.js
@@ -4,8 +4,41 @@ Facebook API handler
   post docs: https://developers.facebook.com/docs/pages-api/posts/
 */
 
-const { handleChannelApiCall } = require("../helpers/channelApi");
 const { constants } = require("../helpers/constants");
+
+// TODO => move this to seperate file later =========================
+const channelEndpointMap = {
+  facebook: constants.fbGraphApiBaseUrl,
+};
+
+async function handleChannelApiCall({ method, endpoint, channel, bearerToken, body, queryParamString = "" }) {
+  const baseUrl = channelEndpointMap[channel];
+
+  if (!baseUrl) {
+    throw new Error("Unsupported Channel");
+  }
+
+  const url = `${baseUrl}/${endpoint}?${queryParamString}`;
+
+  const requestOptions = {
+    method: method || "get",
+    headers: {
+      "Content-Type": "application/json",
+    },
+  };
+
+  if (body) {
+    requestOptions.body = JSON.stringify(body);
+  }
+
+  if (bearerToken) {
+    requestOptions.headers["Authorization"] = `Bearer ${bearerToken}`;
+  }
+
+  const response = await fetch(url, requestOptions);
+  return response;
+}
+// ==========================================================================
 
 async function createPost(pageId, body, pageAccessToken) {
   if (!pageId || !body || !pageAccessToken) {
